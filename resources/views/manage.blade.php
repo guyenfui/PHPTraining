c7014fccd96700b052ba159f51d1e27fd71716b4
guyenfui <huynm1103@gmail.com>
2020-10-15 19:16:16 +0900

    Create fake data and fix mail body

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -28,44 +28,6 @@
 <!-- Page Wrapper -->
 <div id="wrapper">
 
-    <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-        <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
-            </div>
-            <div class="sidebar-brand-text mx-3">管理サイト</div>
-        </a>
-
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Login Screens:</h6>
-                    <a class="collapse-item" href="login.html">Login</a>
-                    <a class="collapse-item" href="register.html">Register</a>
-                    <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                    <div class="collapse-divider"></div>
-                    <h6 class="collapse-header">Other Pages:</h6>
-                    <a class="collapse-item" href="404.html">404 Page</a>
-                    <a class="collapse-item" href="blank.html">Blank Page</a>
-                </div>
-            </div>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <!-- Sidebar Toggler (Sidebar) -->
-        <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
-
-    </ul>
-    <!-- End of Sidebar -->
-
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
@@ -147,7 +109,7 @@
                                     <th>住所</th>
                                     <th>連絡方法</th>
                                     <th>性別</th>
-                                    <th>内容</th>
+                                    <th>本文</th>
                                     <th>登録日時</th>
                                 </tr>
                                 </thead>
@@ -175,6 +137,9 @@
             <div align="center">
                 <a href="{{ route('export.excel') }}" class="btn btn-success">エクセルでダウンロード</a>
                 <a href="{{ route('export.csv') }}" class="btn btn-success">CSVでダウンロード</a>
+            </div>
+            <div align="center">
+                <p style="font-size: smaller">※エクセルファイルはUTF-8のエンコードで開いてください。</p>
             </div>
 
             <!-- /.container-fluid -->
